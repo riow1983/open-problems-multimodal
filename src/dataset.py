c874ccd4f1e151779efd6e46444b59f887bf5611
Ryosuke Horiuchi <ryoiryojoho@gmail.com>
2022-09-23 22:58:23 +0900

    [Debug] exp001

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,13 @@
 from torch.utils.data import Dataset
 
 class TrainDataset(Dataset):
-    def __init__(self, X_data_fold, y_data_fold):
+    def __init__(self, X_data_fold, Y_data_fold):
         self.X_data = X_data_fold
-        self.y_data = y_data_fold
-
-    def __getitem__(self, item):
-        return self.X_data[item], self.y_data[item]
+        self.Y_data = Y_data_fold
 
     def __len__(self):
         return len(self.X_data)
+    
+    def __getitem__(self, item):
+        return self.X_data[item], self.Y_data[item]
+
